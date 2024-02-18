54073ccb04787aee05660c3c2a749313d4ae3e50
Kamala Kanta rout <54476924+kamala127@users.noreply.github.com>
2024-02-18 15:22:35 +0530

    Add files via upload

--- cakeorder/src/main/java/com/cakeorder/controller/UserController.java
+++ cakeorder/src/main/java/com/cakeorder/controller/UserController.java
@@ -3,6 +3,7 @@ package com.cakeorder.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cakeorder.Entity.User;
 import com.cakeorder.dao.Userdao;
 
+@CrossOrigin (origins="*",allowedHeaders="*")
 @RestController
 public class UserController {
 
